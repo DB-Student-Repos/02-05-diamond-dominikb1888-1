2b22ef644662040d5bded1d9346ad5b1cd689787
Dominik Boehler <dominik.boehler@gmx.net>
2023-04-17 17:10:43 +0200

    solved

--- diamond.py
+++ diamond.py
@@ -1,2 +1,13 @@
+from string import ascii_uppercase as letters
+
 def rows(letter):
-    pass
+    u = letters.index(letter) + 1
+    result = []
+    for char in letters[:u]:
+        s = letters.index(char)
+        ap = 2 * u - 1
+        p = (u - s)
+        line = f"{char: ^{ap}}" if char == 'A' else f"{char: >{p}}{' ':{s*2-1}}{char: <{p}}"
+        result.append(line)
+
+    return result[:-1] + result[::-1]
